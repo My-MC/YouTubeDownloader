70c5df802c005404bfd3d0ae6c1000cc52e95745
My <84212641+My-MC@users.noreply.github.com>
2022-01-23 15:17:12 +0900

    [add]Add build file and icon

---
+++ build.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import py2exe
+
+option = {"compressed": 1, "optimize": 2, "bundle_files": 3, }
+# bundle_filesを3にしておくと、exe自体のファイルサイズを小さく抑えることができます。
+
+setup(option={'py2exe': option},
+      windows=[{'script': 'main.py', 'icon_resources': 'ico/icon.ico'}])
+# windowsにすると、コンソールが表示されずにプログラムが実行されます
+# icon_resourcesでexeのiconにしたいiconファイルを指定します。
+# zipfileを設定することにより、ディレクトリ配下にlibというフォルダを作成し、
+# ライブラリを固めてくれるので、見た目がよくなります。
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='YoutubeDownloader',
     version='1.0',
     packages=[''],
-    install_requires=["yt-dlp", "PySimpleGUI"],
+    install_requires=["yt-dlp", "PySimpleGUI, py2win"],
     url='https://github.com/My-MC/YoutubeDownloader',
     license='MIT License',
     author='My-MC',
