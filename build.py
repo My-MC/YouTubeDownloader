58e926af2727653668e3b36f92bb6db2a1709a54
My <84212641+My-MC@users.noreply.github.com>
2022-01-23 15:47:57 +0900

    [fix]It didn't work, so I fixed it

--- build.py
+++ build.py
@@ -1,13 +1,19 @@
-# 参考 http://silence2you.blogspot.com/2013/05/py2exeexe.html
+# 参考 https://www.python-izm.com/third_party/executable/py2exe/
 from distutils.core import setup
 import py2exe
-
-option = {"compressed": 1, "optimize": 2, "bundle_files": 3, }
-# bundle_filesを3にしておくと、exe自体のファイルサイズを小さく抑えることができます。
-
-setup(option={'py2exe': option},
-      windows=[{'script': 'main.py', 'icon_resources': 'ico/icon.ico'}])
-# windowsにすると、コンソールが表示されずにプログラムが実行されます
-# icon_resourcesでexeのiconにしたいiconファイルを指定します。
-# zipfileを設定することにより、ディレクトリ配下にlibというフォルダを作成し、
-# ライブラリを固めてくれるので、見た目がよくなります。
+ 
+option = {
+    'compressed': 1,
+    'optimize': 2,
+    'bundle_files': 1,
+}
+ 
+setup(
+    options = {
+        'py2exe': option,
+    },
+    console = [
+        {'script': 'main.py'}
+    ],
+    zipfile = None,
+)
