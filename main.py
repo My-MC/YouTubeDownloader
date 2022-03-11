057d484680618e195c60906ae29c88ef5a0e2f0d
My <84212641+My-MC@users.noreply.github.com>
2022-03-11 18:47:59 +0900

    Added Setup Setupを追加しました

--- main.py
+++ main.py
@@ -6,44 +6,50 @@ import downloader.mp3 as mp3
 import downloader.webm as webm
 import downloader.best as best
 
-Sg.theme("default1")
 
-combo_val = ["best", "標準", "mp3", "wav"]
+def main():
+    Sg.theme("default1")
 
-myappid = 'my_mc.youtubedownloader.1_0'
-ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+    combo_val = ["best", "標準", "mp3", "wav"]
 
-icon = base64.b64encode(open(r"icon.png", "rb").read())
+    myappid = "my_mc.youtubedownloader.1_0"
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
-title = "Youtube Downloader"
-layout = [
-    [Sg.Text("URL : "), Sg.InputText(key="-URL-")],
-    [Sg.Combo(combo_val, default_value="選択してください", size=(20, 1), key="-TYPE-")],
-    [Sg.Button("Download", key="-ENTER-")],
-    [Sg.Text(key="-RESULT-", size=(20, 1))],
-    [Sg.Output(size=(50, 10))],
-]
-window = Sg.Window(title, layout, size=(400, 300), icon=icon, finalize=True)
+    icon = base64.b64encode(open(r"icon.png", "rb").read())
 
-while True:
-    event, values = window.read()
-    if event == "-ENTER-":
-        window["-RESULT-"].update("Now downloading")
-        print(values["-URL-"])
-        url = values["-URL-"]
-        finish_msg = "Download was finished"
-        if values["-TYPE-"] == "wav":
-            wav.wav(url)
-            window["-RESULT-"].update(finish_msg)
-        if values["-TYPE-"] == "mp3":
-            mp3.mp3(url)
-            window["-RESULT-"].update(finish_msg)
-        if values["-TYPE-"] == "標準":
-            webm.webm(url)
-            window["-RESULT-"].update(finish_msg)
-        if values["-TYPE-"] == "best":
-            best.best(url)
-            window["-RESULT-"].update(finish_msg)
+    title = "Youtube Downloader"
+    layout = [
+        [Sg.Text("URL : "), Sg.InputText(key="-URL-")],
+        [Sg.Combo(combo_val, default_value="選択してください", size=(20, 1), key="-TYPE-")],
+        [Sg.Button("Download", key="-ENTER-")],
+        [Sg.Text(key="-RESULT-", size=(20, 1))],
+        [Sg.Output(size=(50, 10))],
+    ]
+    window = Sg.Window(title, layout, size=(400, 300), icon=icon, finalize=True)
 
-    if event == Sg.WINDOW_CLOSED:
-        break
+    while True:
+        event, values = window.read()
+        if event == "-ENTER-":
+            window["-RESULT-"].update("Now downloading")
+            print(values["-URL-"])
+            url = values["-URL-"]
+            finish_msg = "Download was finished"
+            if values["-TYPE-"] == "wav":
+                wav.wav(url)
+                window["-RESULT-"].update(finish_msg)
+            elif values["-TYPE-"] == "mp3":
+                mp3.mp3(url)
+                window["-RESULT-"].update(finish_msg)
+            elif values["-TYPE-"] == "標準":
+                webm.webm(url)
+                window["-RESULT-"].update(finish_msg)
+            elif values["-TYPE-"] == "best":
+                best.best(url)
+                window["-RESULT-"].update(finish_msg)
+
+        if event == Sg.WINDOW_CLOSED:
+            break
+
+
+if __name__ == "__main__":
+    main()
---
+++ run.py
@@ -0,0 +1,70 @@
+from main import main
+import yaml
+import ctypes
+import base64
+import urllib.request
+import shutil
+import os
+from distutils.dir_util import copy_tree
+import PySimpleGUI as Sg
+
+with open("conf/conf.yml", "r") as f:
+    x = yaml.safe_load(f)
+    if x["installed"] == None:
+        Sg.theme("default1")
+
+        myappid = "my_mc.youtubedownloader.1_0"
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
+
+        icon = base64.b64encode(open(r"icon.png", "rb").read())
+
+        title = "Youtube Downloader Setup"
+        layout = [
+            [Sg.Text("Do you have FFmpeg installed?")],
+            [Sg.Text("FFmpegはインストールされていますか？")],
+            [Sg.Button("Yes", key="-Yes-"), Sg.Button("No, I don't know", key="-No-")],
+            [Sg.Button("Finish", key="-Finish-")],
+            [Sg.Text(key="-RESULT-")],
+            [Sg.Output(size=(50, 10))],
+        ]
+        window = Sg.Window(title, layout, size=(400, 300), icon=icon, finalize=True)
+
+        while True:
+            event, values = window.read()
+
+            def installed():
+                with open("conf/conf.yml", "w") as w:
+                    yw = {"installed": "true"}
+                    yaml.dump(yw, w, encoding="utf-8", allow_unicode=True)
+
+            if event == "-Yes-":
+                window["-Yes-"].update(" ")
+                installed()
+                window["-RESULT-"].update(
+                    "Finished setting up. Please press Finish button"
+                )
+
+            elif event == "-No-":
+                window["-RESULT-"].update("Now setting up")
+                window["-Yes-"].update(" ")
+                window["-No-"].update(" ")
+                url = "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n5.0-latest-win64-lgpl-5.0.zip"
+                save_name = "FFmpeg.zip"
+                os.mkdir("cache")
+                urllib.request.urlretrieve(url, save_name)
+                shutil.unpack_archive("FFmpeg.zip", "cache")
+                copy_tree("./cache/ffmpeg-n5.0-latest-win64-lgpl-5.0/bin", "./")
+                shutil.rmtree("./cache")
+
+                window["-RESULT-"].update(
+                    "Finished setting up. Please press Finish button"
+                )
+                installed()
+
+            elif event == "-Finish-":
+                break
+
+            if event == Sg.WINDOW_CLOSED:
+                break
+    if x["installed"] == "true":
+        main()
