4be7c5676e5ca94302ffe364d8c8746457bcf496
My <84212641+My-MC@users.noreply.github.com>
2022-03-11 20:00:24 +0900

    Update Version

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name="YoutubeDownloader",
-    version="1.0",
+    version="1.1",
     packages=[""],
-    install_requires=["yt-dlp", "PySimpleGUI", "cx_Freeze", "black", "pytest"],
+    install_requires=["yt-dlp", "PySimpleGUI", "cx_Freeze", "black", "pytest", "PyYAML"],
     url="https://github.com/My-MC/YoutubeDownloader",
     license="MIT License",
     author="My-MC",
